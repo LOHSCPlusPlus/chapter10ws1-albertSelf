a9df390ea87704473f6ff68d7a1918dab7c57e50
ALBERTSELF <>
2022-03-09 20:53:51 +0000

    WS1

--- Explore1.cpp
+++ Explore1.cpp
@@ -12,19 +12,22 @@ int main() {
 
 
     // First instance of Point, named a - starts at default 0.0, 0.0
-    //Point b(4.5, 7.8);
-
-    //cout << "Point a: x = " << a.getX() << ", y = " << a.getY() << endl;
-    //cout << "Point b: x = " << b.getX() << ", y = " << b.getY() << endl;
-
-    //a.setX(53);
+    Point b(4.5, 7.8);
+    cout << "Point a:";
+    a.print();
+    cout << " ";
+    cout << "Point b: x = ";
+    b.print();
+    cout << endl;
+    cout << a.calcDistance(b);
+   // a.setX(53);
     //a.setY(51);
-    //cout << "Point a: x = " << a.getX() << ", y = " << a.getY() << endl;
+    cout << "Point a: x = " << a.getX() << ", y = " << a.getY() << endl;
 
-    //cout << "Point a:";
-    //a.print();
-    //cout << "Point b:";
-    //b.print(); 
+    cout << "Point a:";
+    a.print();
+    cout << "Point b:";
+    b.print(); 
 
     // Look! the equal operator still works!
     // a = b;
