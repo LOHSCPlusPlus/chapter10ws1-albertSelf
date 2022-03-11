1f2f1ed843aadd5395e735a110701a1146b68e67
ALBERTSELF <>
2022-03-11 17:35:41 +0000

    WS 2 complete

--- Line.cpp
+++ Line.cpp
@@ -1,1 +1,21 @@
 #include "Line.h"
+#include <iostream>
+
+using namespace std;
+
+Line::Line(Point s, Point e) {
+start = s;
+end = e;
+}
+
+void Line::PrintLine() {
+  cout << " Start";
+  start.print();
+  cout << " End";
+  end.print();
+  cout << endl;
+}
+
+double Line::lineLength() {
+  return(start.calcDistance(end));
+}
--- LineClient.cpp
+++ LineClient.cpp
@@ -1,5 +1,14 @@
 #include "Line.h"
+#include <iostream>
+
+using namespace std;
 
 int main() {
+
+  Point P1(2.4, 5.2);
+  Point P2(5.3, 8.9);
+  Line one(P1, P2);
+  one.PrintLine();
+  cout << one.lineLength();
     return 0;
 }
